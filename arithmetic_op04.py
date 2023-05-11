8efad3068c72e6125ebc8f405381c16bb577a1c1
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-11 18:04:45 +0500

    vazifa 4

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -9,3 +9,10 @@
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
 
 #Print the value of the 'answer.
+
+n = 234
+a = n%10
+b = n//10
+s = b%10
+d = n//100
+print((a*3)+(s*2)+(d*1))
